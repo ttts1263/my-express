932a6357b903c327e957702dfe3ac43871a7dcac
hoodie <popping2606@gmail.com>
2024-01-11 20:37:38 +0900

    feat: add verify token

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 import { myNoteRouter } from './myNote.js'
 
 export const app = express()
+
+app.use(bodyParser.json())
+
 app.use(
   cors({
     origin: 'http://localhost:5173', // FrontEnd
@@ -10,33 +14,6 @@ app.use(
 )
 
 app.use('/my-note', myNoteRouter)
-
-// RESTful api 메소드 추가 (get(조회), post(생성), put(덮어쓰기), patch(업데이트), delete(삭제))
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/about', (req, res) => {
-  res.send('This is about page!')
-})
-
-app.post('/post', (req, res) => {
-  const newPostId = 1
-  res.send({ id: newPostId })
-})
-
-app.post('/login', (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
-  // DB에서 email 과 password가 일치하는지 체크
-  // 일치하지 않으면 401 Unauthorized
-  // 일치하면 200 OK와 사용자 정보를 응답
-})
-
-app.post('/my-note/jwt', (req, res) => {
-  console.log(req.body)
-  res.send('jwt 저장 완료')
-})
 
 const port = 3000
 app.listen(port, () => {
--- myNote.js
+++ myNote.js
@@ -1,8 +1,32 @@
 import { Router } from 'express'
+import { OAuth2Client } from 'google-auth-library'
 
 export const myNoteRouter = Router()
 
-myNoteRouter.post('/jwt', (req, res) => {
+myNoteRouter.post('/jwt', async (req, res) => {
   console.log(req.body)
-  res.send('jwt 저장 완료')
+  const jwt = req.body.jwt
+
+  // jwt 검증
+  const client = new OAuth2Client()
+  const verified = await client.verifyIdToken({
+    idToken: jwt,
+  })
+  const payload = verified.getPayload()
+  console.log(payload)
+
+  // TODO: 액세스 토큰을 만들어서 프론트 쿠키에 저장하기
+  const accessToken = 'token'
+
+  res.cookie('my-note-session', accessToken, {
+    httpOnly: true, // 백엔드에서만 쿠키 조작 가능
+    sameSite: 'none', // cors 에서도 쿠키 전송 가능
+    // secure: true, // https 에서만 동작
+  })
+
+  res.send({ accessToken, payload })
 })
+
+// 구글로그인 > 유저정보(credential - JWT) > 백엔드로 보내기 >
+// 백엔드에서 credential 검증 > 프론트에서 보낸 jwt 유효한지 파악 >
+// 구글이 검증해줘야함. > google-auth-library 설치 > 검증
