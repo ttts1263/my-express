df0a676fe57a8bf6e53fb87b0035bd75e5c42754
hoodie <popping2606@gmail.com>
2024-01-19 19:33:45 +0900

    feat: save session in cookie

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
 import { myNoteRouter } from './myNote.js'
 
 export const app = express()
 
+dotenv.config()
 app.use(bodyParser.json())
-
+app.use(cookieParser())
 app.use(
   cors({
     origin: 'http://localhost:5173', // FrontEnd
--- myNote.js
+++ myNote.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import { OAuth2Client } from 'google-auth-library'
+import jsonwebtoken from 'jsonwebtoken'
 
 export const myNoteRouter = Router()
 
 myNoteRouter.post('/jwt', async (req, res) => {
-  console.log(req.body)
   const jwt = req.body.jwt
 
   // jwt 검증
@@ -13,20 +13,38 @@ myNoteRouter.post('/jwt', async (req, res) => {
     idToken: jwt,
   })
   const payload = verified.getPayload()
-  console.log(payload)
 
-  // TODO: 액세스 토큰을 만들어서 프론트 쿠키에 저장하기
-  const accessToken = 'token'
+  // 세션 생성
+  const userData = {
+    email: payload.email,
+    name: payload.name,
+    picture: payload.picture,
+  }
+  const session = jsonwebtoken.sign(userData, process.env.JWT_SECRET) // HS256
 
-  res.cookie('my-note-session', accessToken, {
-    httpOnly: true, // 백엔드에서만 쿠키 조작 가능
+  // 프론트 쿠키 전송
+  res.cookie('my-note-session', session, {
+    maxAge: 1000 * 60 * 60 * 24 * 365, // 1년
+    // httpOnly: true, // 백엔드에서만 쿠키 조작 가능
     sameSite: 'none', // cors 에서도 쿠키 전송 가능
     // secure: true, // https 에서만 동작
   })
 
-  res.send({ accessToken, payload })
+  res.send({ session, userData })
 })
 
 // 구글로그인 > 유저정보(credential - JWT) > 백엔드로 보내기 >
 // 백엔드에서 credential 검증 > 프론트에서 보낸 jwt 유효한지 파악 >
 // 구글이 검증해줘야함. > google-auth-library 설치 > 검증
+
+myNoteRouter.get('/sessionCheck', (req, res) => {
+  const session = req.cookies['my-note-session']
+  if (!session) return res.send({ session: false })
+
+  try {
+    const userData = jsonwebtoken.verify(session, process.env.JWT_SECRET)
+    res.send({ session: true, userData })
+  } catch (e) {
+    res.send({ session: false })
+  }
+})
